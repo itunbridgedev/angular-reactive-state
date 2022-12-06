deb98db7dbbc82e77ef13a5f260418fc6398d228
Ian Tunbridge <ian.tunbridge@imparnter.com>
2022-12-06 10:22:30 -0700

    add missing files

---
+++ src/app/admin/admin.routes.ts
@@ -0,0 +1,29 @@
+import { RouterModule, Routes } from '@angular/router';
+
+// routes
+export const AdminRoutes: Routes = [
+  {
+    path: 'donuts',
+    loadComponent: () =>
+      import('../admin/containers/donut-list/donut-list.component').then(
+        (x) => x.DonutListComponent
+      ),
+  },
+  {
+    path: 'donuts/new',
+    loadComponent: () =>
+      import('../admin/containers/donut-item/donut-item.component').then(
+        (x) => x.DonutItemComponent
+      ),
+    data: { isEdit: false },
+  },
+  {
+    path: 'donuts/:id',
+    loadComponent: () =>
+      import('../admin/containers/donut-item/donut-item.component').then(
+        (x) => x.DonutItemComponent
+      ),
+    data: { isEdit: true },
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'donuts' },
+];
